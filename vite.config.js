f6cfac16a89c00278b77c28f393c2589c8740176
Stefan Peters <Stefan.Peters@gbv.de>
2024-10-02 11:07:15 +0200

    Migrate from Vue CLI to Vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue2"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  plugins: [
+    vue(),
+  ],
+  server: {
+    port: 8093,
+  },
+})
